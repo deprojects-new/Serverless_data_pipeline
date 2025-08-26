ef5b95f84f1620023bc26c9afc25c9581ec1f118
A Abhinav Jaisiv <aryanabhinav01@gmail.com>
2025-08-25 21:37:50 -0400

    fixed OIDC

--- terraform/iam_oidc.tf
+++ terraform/iam_oidc.tf
@@ -3,10 +3,10 @@
 data "aws_caller_identity" "current" {}
 
 resource "aws_iam_openid_connect_provider" "github" {
-  count            = var.enable_ci_bootstrap ? 1 : 0
-  url              = "https://token.actions.githubusercontent.com"
-  client_id_list   = ["sts.amazonaws.com"]
-  thumbprint_list  = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+  count           = var.enable_ci_bootstrap ? 1 : 0
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 
   lifecycle {
     ignore_changes = [tags, tags_all]
